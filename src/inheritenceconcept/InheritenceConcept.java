85a230a717e7f90c03928755d367aaa36ae354e8
Dan <Dan@Centurion.attlocal.net>
2015-09-03 00:59:00 -0500

    Added last instance check for MagicPower and LightningStrike

--- src/inheritenceconcept/InheritenceConcept.java
+++ src/inheritenceconcept/InheritenceConcept.java
@@ -5,6 +5,8 @@ public class InheritenceConcept {
     public static void main(String[] args) {
         GamingPC pc1 = new GamingPC();
         System.out.println(pc1 instanceof Computer);
+        LightningStrike lightStrike1 = new LightningStrike();
+        System.out.println(lightStrike1 instanceof MagicPower);
     }
     
 }
