98b2d9a3b183ca6705c86e20dc17b49646b5bc85
Dan <Dan@Centurion.attlocal.net>
2015-09-03 00:41:25 -0500

    Deleted computer interface, changed to computer abstract class Added isOn boolean and on/off methods to computer abstract class

--- src/inheritenceconcept/Computer.java
+++ src/inheritenceconcept/Computer.java
@@ -1,5 +1,17 @@
 package inheritenceconcept;
 
-public interface Computer {
+public abstract class Computer {
+    private boolean isOn;
     
+    public void turnOn(){
+        if(isOn){
+            System.out.println("Already on");
+        }else{
+            System.out.println("Hello world");
+        }
+    }
+    
+    public void turnOff(){
+        System.out.println("Goodbye");
+    }
 }
